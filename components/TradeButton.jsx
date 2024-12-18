e2dbb60584197e4cb7902eb45308cae0e1a6eb73
Akhil BG <83879587+akhilbhadrangirija@users.noreply.github.com>
2024-12-18 14:41:14 +0530

    Fix trade buttton unauth state

--- components/TradeButton.jsx
+++ components/TradeButton.jsx
@@ -11,8 +11,11 @@ import React, { useEffect, useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import BuySell from '@/components/BuySell'
+import { DialogDescription } from '@radix-ui/react-dialog'
 import Image from 'next/image'
+import Link from 'next/link'
 import { cn } from '@/lib/utils'
+import useAuth from '@/lib/useAuth'
 
 // import { balanceOf, balanceOfUsdc } from '@/lib/contract-utils'
 
@@ -30,7 +33,7 @@ function TradeButton({
       : ctaText
   const isDisabled =
     data.status === 'unlisted' || undefined ? true : data?.paused ? true : false
-  // const { address, isConnected } = useAccount()
+  const { isAuthenticate } = useAuth()
   const [action, setAction] = useState('')
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const [balance, setBalance] = useState(0)
@@ -43,17 +46,8 @@ function TradeButton({
       setAction('')
     }
   }, [isDialogOpen])
-
-  return (
-    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-      <DialogTrigger asChild>
-        <Button
-          variant={variant}
-          disabled={isDisabled}
-          className={cn('my-auto w-20', className)}>
-          {cta}
-        </Button>
-      </DialogTrigger>
+  function AuthContent() {
+    return (
       <DialogContent className="sm:max-w-[425px] bg-white">
         <DialogHeader>
           <DialogTitle className="text-2xl font-medium">Trade</DialogTitle>
@@ -115,8 +109,39 @@ function TradeButton({
           )}
         </div>
       </DialogContent>
+    )
+  }
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant={variant}
+          disabled={isDisabled}
+          className={cn('my-auto w-20', className)}>
+          {cta}
+        </Button>
+      </DialogTrigger>
+      {isAuthenticate ? <AuthContent /> : <LoginDialog />}
     </Dialog>
   )
 }
-
+function LoginDialog() {
+  return (
+    <DialogContent className="sm:max-w-[425px] bg-white">
+      <DialogHeader>
+        <DialogTitle className="text-2xl font-medium">
+          Please Sign in!
+        </DialogTitle>
+      </DialogHeader>
+      <DialogDescription>
+        You need to Sign in first to perform any trade?
+      </DialogDescription>
+      <div className="flex flex-col gap-4">
+        <Link href="/sign-in">
+          <Button className="w-full">Sign in</Button>
+        </Link>
+      </div>
+    </DialogContent>
+  )
+}
 export default TradeButton
